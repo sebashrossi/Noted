313cfc61c18fb80dd3a210c0b06d0bb59d999418
Sebas Rossi <sebashrossi@gmail.com>
2021-08-19 16:46:33 +0200

    changed background color

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,7 +14,7 @@ function Note(props) {
       </div>
       <div className="delete-button">
         <button onClick={handleClick}>
-          <DeleteIcon style={{ color: "#4aDEDE" }} />
+          <DeleteIcon style={{ color: "linear-gradient(180deg, rgba(140,74,242,1) 8%, rgba(109,85,245,1) 88%);" }} />
         </button>
       </div>
     </div>
